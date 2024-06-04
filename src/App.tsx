44a5419e9bce9ea8f5dcbd3a4d71d5c941f25db5
rashidpbi <rashidpbi786@gmail.com>
2024-06-04 22:44:43 +0530

    type added for Launch

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import { getLauches } from './Api';
+import { Launch } from './types';
 function App() {
 
   const getLaunchData = async() =>{
-const data = await getLauches();
-console.log(data)
+const result = await getLauches();
+let Launch:Launch[] = result.data;
+console.log(Launch)
+
   }
+  
   useEffect(()=>{
 getLaunchData();
   },[])
