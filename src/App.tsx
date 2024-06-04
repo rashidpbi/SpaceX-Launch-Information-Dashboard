0f935ba2ab25718004ed1ddbf0d3732564f579d0
rashidpbi <rashidpbi786@gmail.com>
2024-06-04 23:01:58 +0530

    checked rendering mission names to console

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { getLauches } from './Api';
 import { Launch } from './types';
 function App() {
-
+const [LaunchData,setLaunchData] = useState<Launch[]>([]);
   const getLaunchData = async() =>{
 const result = await getLauches();
 let Launch:Launch[] = result.data;
 console.log(Launch)
+return Launch;
 
   }
   
-  useEffect(()=>{
-getLaunchData();
+  useEffect( ()=>{
+    async function fetchData(){
+
+      const LaunchData = await getLaunchData();
+      setLaunchData(LaunchData);
+      
+    }
+   fetchData();
   },[])
 
   return (
     <div className="App">
-   Hi
+  {LaunchData.map((launch)=>launch.mission_name)}
     </div>
   );
 }
