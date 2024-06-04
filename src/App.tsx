499a1a4efd5d022187dc18d546ee9e1f40b31a5b
rashidpbi <rashidpbi786@gmail.com>
2024-06-04 21:08:05 +0530

    sample data logged in console

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-
+import { getLauches } from './Api';
 function App() {
 
+  const getLaunchData = async() =>{
+const data = await getLauches();
+console.log(data)
+  }
+  useEffect(()=>{
+getLaunchData();
+  },[])
 
   return (
     <div className="App">
-    hi
+   Hi
     </div>
   );
 }
