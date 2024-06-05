a256fb6960d024cafd4187c1c5f79daca7dc666d
rashidpbi <rashidpbi786@gmail.com>
2024-06-05 13:15:11 +0530

    tested sample list based on launch status

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import { LaunchProvider } from "./context/LaunchContext";
 import Filters from "./components/Filters";
+import LaunchList from "./components/LaunchList";
 function App() {
  
   return (<LaunchProvider>
 
 <div className="App"><div>
     
-    <Filters/></div></div>
+    <Filters/><LaunchList/></div></div>
   </LaunchProvider>);
 }
 
---
+++ src/components/LaunchList.tsx
@@ -0,0 +1,43 @@
+import React, { useContext, useState } from 'react';
+import { LaunchContext } from '../context/LaunchContext';
+
+const LaunchList = () => {
+  const context = useContext(LaunchContext);
+
+  if (!context) return <div>Loading...</div>;
+
+  const { filteredLaunches, loading } = context;
+
+  if (loading) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>No:</th>
+            <th>Launched (UTC)</th>
+            <th>Location</th>
+            <th>Mission</th>
+            <th>Launch Status</th>
+            <th>Rocket</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredLaunches.map((launch,index) => (
+            <tr key={index}>
+              <td>{launch.flight_number}</td>
+              <td>{new Date(launch.launch_date_utc).toUTCString()}</td>
+              <td>{launch.launch_site.site_name}</td>
+              <td>{launch.mission_name}</td>
+              <td>{launch.launch_success ? 'Success' : launch.upcoming ? 'Upcoming' : 'Failure'}</td>
+              <td>{launch.rocket.rocket_name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default LaunchList;
