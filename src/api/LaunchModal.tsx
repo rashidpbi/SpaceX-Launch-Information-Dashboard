0549458ad05abbb9851e991501e2ec3b654040dc
rashidpbi <rashidpbi786@gmail.com>
2024-06-15 13:35:42 +0530

    basic styling

--- src/api/LaunchModal.tsx
+++ src/api/LaunchModal.tsx
@@ -16,7 +16,7 @@ const LaunchModal = ({ launch }: LaunchModalProps) => {
     <div className="">
       <div className="">
         
-        <div className="flex">
+        <div className="flex m-4">
           
           <div>
             <img
@@ -25,33 +25,33 @@ const LaunchModal = ({ launch }: LaunchModalProps) => {
               style={{ height: 150, width: 150 }}
             />
           </div>
-          <div className="grid">
+          <div className="grid place-content-between ml-4 ">
             <div className="flex">
               <h2>{launch.mission_name}</h2>
-              <p>
+              <div className="ml-2">
                
                 {launch.launch_success
-                  ? <div className="text-green-900 bg-green-100  rounded-3xl w-20 justify-center font-semibold">
-                  Success
+                  ? <div className="flex text-green-900 bg-green-100  rounded-3xl w-20 h-6 items-center  justify-center font-semibold">
+                  Success 
                 </div>
                   : launch.upcoming
-                  ? <div className="text-amber-800 bg-yellow-100 rounded-3xl w-24 justify-center font-semibold">
+                  ? <div className="flex text-amber-800 bg-yellow-100 rounded-3xl w-28 h-6 items-center  justify-center font-semibold">
                   upcoming
                 </div>
-                  : <div className="text-rose-800 bg-rose-200 rounded-3xl w-16 justify-center font-semibold">
+                  : <div className=" flex text-rose-800 bg-rose-200 rounded-3xl w-16 h-6 items-center  justify-center font-semibold">
                   failure
                 </div>}
-              </p>
+              </div>
             </div>
-            <p>Rocket: {launch.rocket.rocket_name}</p>
+            <div>Rocket: {launch.rocket.rocket_name}</div>
             <div className="flex" >
-              <a href={wiki_url} title="wiki" target="_blank" rel="noreferrer">
+              <a href={nasa_src} title="wiki" target="_blank" rel="noreferrer">
                 <img src={nasa_img_url} style={{ height: 30, width: 30 }} alt="" />
               </a>
               <a href={wiki_url} title="wiki" target="_blank" rel="noreferrer">
                 <img src={wiki_img_url} style={{ height: 30, width: 30 }} alt="" />
               </a>
-              <a href={nasa_src} title="yt" target="_blank" rel="noreferrer">
+              <a href={yt_url} title="yt" target="_blank" rel="noreferrer">
                 <img src={yt_img_url} style={{ height: 30, width: 30 }} alt="" />
               </a>
             </div>
@@ -59,31 +59,35 @@ const LaunchModal = ({ launch }: LaunchModalProps) => {
           </div>
         </div>
 
-        <p>
-          Status: {launch.details}{" "}
-          <a href={wiki_url} target="_blank" rel="noreferrer">
-            wikipedia
-          </a>{" "}
-        </p>
-        <p>flight number: {launch.flight_number}</p>
-        <p>mission name:{launch.mission_name}</p>
-        <p>rocket type:{launch.rocket.rocket_type}</p>
-        <p>
-          manufacturer:{launch.rocket?.second_stage?.payloads?.[0].manufacturer}
-        </p>
-        <p>
-          nationality:{launch.rocket?.second_stage?.payloads?.[0].nationality}
-        </p>
-        <p>Launched Date: {new Date(launch.launch_date_utc).toUTCString()}</p>
-        <p>
-          payload type:{launch.rocket?.second_stage?.payloads?.[0].payload_type}
-        </p>
-        <p>
-          orbit:{" "}
-          {launch.rocket?.second_stage?.payloads?.[1]?.orbit || "Unknown"}
-        </p>
-        <p>launch site: {launch.launch_site.site_name}</p>
-      </div>
+        <div className="m-4 p-4">
+          <div >
+            {launch.details}
+            <a href={wiki_url} target="_blank" rel="noreferrer">
+              <span className="text-blue-800">{" "}Wikipedia</span>
+            </a>
+          </div>
+          <div className="my-4 divide-y divide-solid">
+            <div className="py-2">flight number:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{launch.flight_number}</div>
+            <div className="py-2">mission name:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{launch.mission_name}</div>
+            <div className="py-2">rocket type:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{launch.rocket.rocket_type}</div>
+            <div className="py-2">
+              manufacturer:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{launch.rocket?.second_stage?.payloads?.[0].manufacturer}
+            </div>
+            <div className="py-2">
+              nationality:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{launch.rocket?.second_stage?.payloads?.[0].nationality}
+            </div>
+            <div className="py-2">Launched Date: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{new Date(launch.launch_date_utc).toUTCString()}</div>
+            <div className="py-2">
+              payload type:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{launch.rocket?.second_stage?.payloads?.[0].payload_type}
+            </div>
+            <div className="py-2">
+              orbit:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              {launch.rocket?.second_stage?.payloads?.[1]?.orbit || "Unknown"}
+            </div>
+            <div className="py-2">launch site:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {launch.launch_site.site_name}</div>
+                  </div>
+          </div>
+        </div>
     </div>
   );
 };
